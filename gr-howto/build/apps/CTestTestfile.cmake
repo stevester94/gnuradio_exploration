1488a75c0b956348ab4f8dca112f02d5f320a6f8
Steven Mackey <stevester94@gmail.com>
2021-02-24 19:23:47 -0800

    Babbys first working

--- gr-howto/build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/steven/CSC500/gnuradio_exploration/gr-howto/apps
-# Build directory: /home/steven/CSC500/gnuradio_exploration/gr-howto/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- gr-howto/build/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/steven/CSC500/gnuradio_exploration/gr-howto/lib
-# Build directory: /home/steven/CSC500/gnuradio_exploration/gr-howto/build/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
